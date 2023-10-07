863432175a3365058532e9fb9b8eae028d34d038
Adrian González <adrigondo.profesional@gmail.com>
2023-10-07 16:30:34 -0600

    Update: keyboard functionality

--- Components/key_button.gd
+++ Components/key_button.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 # Expose a variable for the Label text
 @export var letterText:String = "A"
+@export var keyboard:Node2D
 
+signal key_clicked(key:String)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Use get_node to get a reference to the Label node by its full path
@@ -11,10 +13,12 @@ func _ready():
 	if label_node:
 		label_node.text=letterText
 	else:
-		print("Label node not found")
-
-
+		print_debug("Label node not found")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_texture_button_pressed():
+	emit_signal("key_clicked",letterText)
