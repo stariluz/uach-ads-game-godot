7f3df0bd99a106b40f669e87628eace75a8e6c8c
Adrian González <adrigondo.profesional@gmail.com>
2023-10-13 16:48:13 -0600

    Update: update word on try

--- Components/WordGeneration/word_controller.gd
+++ Components/WordGeneration/word_controller.gd
@@ -10,11 +10,13 @@ const words:Array[String]=[
 	"pelota",
 	"conceptos",
 ];
-@export var WORD_WIDTH:float=1520;
-@export var LETTER_FREESPACE_ASPECT_RATIO:float=0.1;
+@export var WORD_WIDTH:float=1520
+@export var LETTER_FREESPACE_ASPECT_RATIO:float=0.1
 @export var currentWord:String=""
 var tryWord:String=""
-var letterBoxUIScene;
+var letterBoxUIScene
+var lettersBoxUIIntances:Array=[]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	letterBoxUIScene=preload("res://Components/WordGeneration/letter_box_ui.tscn");
@@ -45,12 +47,36 @@ func renderWordUI():
 func renderLetterBoxUI(letter:String, horizontalPosition:int, horizontalSize: float):
 	var lbUIInstance = letterBoxUIScene.instantiate()
 	add_child(lbUIInstance)
+	
 	lbUIInstance.get_node("Letter").text=letter;
 	var new_position = Vector2(horizontalPosition, 0)
 	var scaleMultiplier=horizontalSize/lbUIInstance.size.x
 	lbUIInstance.position = new_position
-	print_debug("DEV - size ",lbUIInstance.size)
+#	print_debug("DEV - size ",lbUIInstance.size)
 	
 	lbUIInstance.scale=Vector2(scaleMultiplier, scaleMultiplier)
-	print_debug("DEV - scale ",lbUIInstance.scale)
+#	print_debug("DEV - scale ",lbUIInstance.scale)
 	
+	lettersBoxUIIntances.push_back(lbUIInstance)
+	
+func rerenderLetterBoxUI(index:int, letter:String):
+	print_debug(lettersBoxUIIntances[index])
+	lettersBoxUIIntances[index].get_node("Letter").text=letter
+
+func onTry(key:String):
+	print_debug("TRY", key)
+	var isRightTry=false
+	
+	for letter in tryWord:
+		if letter.to_upper()==key.to_upper():
+			print_debug(letter, " ", key)
+			return
+			
+	for i in range(currentWord.length()):
+		print_debug(currentWord[i], " ", key)
+		if currentWord[i].to_upper()==key.to_upper():
+			tryWord[i]=key;
+			rerenderLetterBoxUI(i, key)
+			isRightTry=true
+
+	return isRightTry
--- Components/keyboard.gd
+++ Components/keyboard.gd
@@ -1,15 +1,14 @@
 extends Control
 
+signal key_clicked(key:String)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
 func _on_a_key_clicked(key:String):
+	emit_signal("key_clicked",key)
 	print_debug(key)
-	pass # Replace with function body.
